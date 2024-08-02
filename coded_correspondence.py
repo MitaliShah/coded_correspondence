06984d07a0b07f56430a1b65669dbc2e25c09189
MitaliShah <mitalishaleenshah@gmail.com>
2024-08-02 08:40:13 -0500

    step 1: solution

--- coded_correspondence.py
+++ coded_correspondence.py
@@ -1,1 +1,17 @@
-print("hello")
+# decode the following message and print the result. This message has an offset of 10
+# xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+message = "xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!zbx"
+
+translated_message = ""
+
+for character in message:   
+  if character in alphabet:
+    character_value = alphabet.find(character)
+    translated_message += alphabet[(character_value + 10) % 26]    
+  else:
+    translated_message += character
+
+print(translated_message)
